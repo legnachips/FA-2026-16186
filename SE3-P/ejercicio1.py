052bae6417380902f0957efb75f0d9f69ef9ba90
legnachips <legna@gmail.com>
2026-04-10 10:36:57 -0500

    subiendo ejercicio1 en py

---
+++ SE3-P/ejercicio1.py
@@ -0,0 +1,8 @@
+n = input("ingrese su nombre: ")
+c = input("ingrese su carrera: ")
+
+print("------- IMPRESION CON + -------");
+print(n + ",Bienvenido a la carrera FA de la carrera " + c);
+
+print("------- IMPRESION CON $ -------");
+print(f"{n}, Bienvenido al curso FA de la carrear {c}");
