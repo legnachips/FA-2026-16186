862f87cdc9ccd895add39a0b2614d691f8213a24
legnachips <legna@gmail.com>
2026-04-10 10:20:27 -0500

    subiendo ejercicio1 en c#

---
+++ SE3-C/ejercicio1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+
+            Console.WriteLine("ingrese nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("\ningrese carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine("------- IMPRESION CON + -------");
+            Console.WriteLine(nombre + ",Bienvenido a la carrera FA de la carrera " + carrera);
+
+            Console.WriteLine("------- IMPRESION CON $ -------");
+            Console.WriteLine($"{nombre}, Bienvenido al curso FA de la carrear {carrera}");
+
+            Console.ReadKey();
+        }
+    }
+}
